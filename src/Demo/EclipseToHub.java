c8687275a1d264e9237aebbd03943deb5629d1c6
Venkey0467 <venkataramana3094@gmail.com>
2023-10-16 12:20:30 +0530

    FirstCommit

---
+++ src/Demo/EclipseToHub.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class EclipseToHub {
+	public static void printName() {
+		System.out.println("Yuvan Surya Sai");
+	}
+
+	public static void main(String[] args) {
+		printName();
+	}
+}
